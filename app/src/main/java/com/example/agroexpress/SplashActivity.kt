c31a22b499ebcbb93b2fc1c9f33ec194b9d1e93c
santiago <santiagoquevedo204777@gmail.com>
2023-03-15 08:36:52 -0500

    carousel

--- app/src/main/java/com/example/agroexpress/SplashActivity.kt
+++ app/src/main/java/com/example/agroexpress/SplashActivity.kt
@@ -3,9 +3,10 @@ package com.example.agroexpress
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.)
     }
 }
