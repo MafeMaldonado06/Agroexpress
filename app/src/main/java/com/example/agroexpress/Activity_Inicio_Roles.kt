09e8e46735ccc30d1655a81044b7f2b4dbea6053
CAPO1481 <caporto8@misena.edu.co>
2023-03-15 12:06:36 -0500

    cambios morris revision

--- app/src/main/java/com/example/agroexpress/Activity_Inicio_Roles.kt
+++ app/src/main/java/com/example/agroexpress/Activity_Inicio_Roles.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.imaginativeworld.whynotimagecarousel.CarouselItem
 import org.imaginativeworld.whynotimagecarousel.ImageCarousel
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
 
 class Activity_Inicio_Roles : AppCompatActivity() {
 
