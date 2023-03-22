42fee6ecff0ac98b2ff2a74e25d18d1f52c062cb
santiago <santiagoquevedo204777@gmail.com>
2023-03-22 08:59:53 -0500

    v7

--- app/src/main/java/com/example/agroexpress/RegistrarFragment.kt
+++ app/src/main/java/com/example/agroexpress/RegistrarFragment.kt
@@ -5,17 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegistrarFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegistrarFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -29,31 +26,26 @@ class RegistrarFragment : Fragment() {
         }
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registrar, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegistrarFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegistrarFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        //return inflater.inflate(R.layout.fragment_cam_editarproducto, container, false)
+        val t = inflater.inflate(R.layout.fragment_registrar, container, false)
+        val spinner = t.findViewById<Spinner>(R.id.paisSpiner)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.planets_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+        return t
     }
 }
