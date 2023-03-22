f6cea963868d81334ea67e7dc325458ac6f9eea9
CAPO1481 <caporto8@misena.edu.co>
2023-03-22 07:30:09 -0500

    cambios morris revision2

--- app/src/main/java/com/example/agroexpress/Activity_Inicio_Roles.kt
+++ app/src/main/java/com/example/agroexpress/Activity_Inicio_Roles.kt
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.imaginativeworld.whynotimagecarousel.CarouselItem
 import org.imaginativeworld.whynotimagecarousel.ImageCarousel
-import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
+
+
 
 class Activity_Inicio_Roles : AppCompatActivity() {
 
-     private val list = mutableListOf<CarouselItem>()
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,10 +17,6 @@ class Activity_Inicio_Roles : AppCompatActivity() {
         setContentView(R.layout.activity_inicio_roles)
 
 
-        val carousel: ImageCarousel = findViewById(R.id.carousel)
-        list.add(CarouselItem("https://www.procapslaboratorios.com/getattachment/Blog/Salud-y-Bienestar/Es-importante-consumir-frutas-y-verduras-1/procaps-frutas-y-verduras-beneficios-1.jpg.aspx?width=715&height=528"))
-        list.add(CarouselItem("https://www.edualimentaria.com/images/frutas-verduras/frutas_hortaliza_verduras.jpg"))
-        list.add(CarouselItem("https://www.cdc.gov/foodsafety/images/comms/features/GettyImages-1247930626-500px.jpg?_=00453"))
-        carousel.addData(list)
+
     }
 }
--- app/src/main/java/com/example/agroexpress/MainActivity.kt
+++ app/src/main/java/com/example/agroexpress/MainActivity.kt
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.imaginativeworld.whynotimagecarousel.CarouselItem
 import org.imaginativeworld.whynotimagecarousel.ImageCarousel
-import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
+
 
 class MainActivity : AppCompatActivity() {
 
     private val list = mutableListOf<CarouselItem>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_roles)
