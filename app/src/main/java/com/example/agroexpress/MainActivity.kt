f7156bae51da16b95595a3f1a7f484e31ff00267
julianc20 <Salazardix321@gmail.com>
2023-03-22 11:21:22 -0500

    kt salaz

--- app/src/main/java/com/example/agroexpress/MainActivity.kt
+++ app/src/main/java/com/example/agroexpress/MainActivity.kt
@@ -2,25 +2,14 @@ package com.example.agroexpress
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.imaginativeworld.whynotimagecarousel.CarouselItem
-import org.imaginativeworld.whynotimagecarousel.ImageCarousel
-
 
 class MainActivity : AppCompatActivity() {
 
-    private val list = mutableListOf<CarouselItem>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_roles)
-
-
-        val carousel: ImageCarousel = findViewById(R.id.carousel)
-        list.add(CarouselItem("https://www.procapslaboratorios.com/getattachment/Blog/Salud-y-Bienestar/Es-importante-consumir-frutas-y-verduras-1/procaps-frutas-y-verduras-beneficios-1.jpg.aspx?width=715&height=528"))
-        list.add(CarouselItem("https://www.edualimentaria.com/images/frutas-verduras/frutas_hortaliza_verduras.jpg"))
-        list.add(CarouselItem("https://www.cdc.gov/foodsafety/images/comms/features/GettyImages-1247930626-500px.jpg?_=00453"))
-        carousel.addData(list)
-
 
     }
 }
